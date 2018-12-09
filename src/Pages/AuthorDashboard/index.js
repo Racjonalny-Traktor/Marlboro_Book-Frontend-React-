0bf31c9abaf4c1de0d6287158b7e04f7ff4d0423
Dominik Ruman <dr124@o2.pl>
2018-12-09 05:07:48 +0100

    author page

--- src/Pages/AuthorDashboard/index.js
+++ src/Pages/AuthorDashboard/index.js
@@ -4,6 +4,10 @@ import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
 
 import {
   Radar,
@@ -21,10 +25,29 @@ import {
   ResponsiveContainer,
   PieChart,
   Pie,
-  Cell
+  Cell,
+  BarChart,
+  Bar
 } from "recharts";
 
 import "./index.scss";
+
+class TriangleBar extends React.Component {
+  getPath(x, y, width, height) {
+    return `M${x},${y + height}
+            C${x + width / 3},${y + height} ${x + width / 2},${y +
+      height / 3} ${x + width / 2}, ${y}
+            C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y +
+      height} ${x + width}, ${y + height}
+            Z`;
+  }
+  render() {
+    const { fill, x, y, width, height } = this.props;
+    return (
+      <path d={this.getPath(x, y, width, height)} stroke="none" fill={fill} />
+    );
+  }
+}
 
 class AuthorDashboard extends React.Component {
   state = {
@@ -126,6 +149,7 @@ class AuthorDashboard extends React.Component {
       name: "Konrad Pękala",
       gender: "M",
       count: 69,
+      image: "https://i.ibb.co/pXsHQkq/bookpic.png",
       history: [
         1,
         0,
@@ -182,7 +206,18 @@ class AuthorDashboard extends React.Component {
   };
 
   render() {
-    const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+    const colors = [
+      "#1f77b4",
+      "#ff7f0e",
+      "#2ca02c",
+      "#d62728",
+      "#9467bd",
+      "#8c564b",
+      "#e377c2",
+      "#7f7f7f",
+      "#bcbd22",
+      "#17becf"
+    ];
     return (
       <div className="author-dashboard">
         <main>
@@ -215,6 +250,13 @@ class AuthorDashboard extends React.Component {
                   Najlepszy czytelnik <br />
                   <small>Twoim największym fanem jest...</small>
                 </h1>
+                <ResponsiveContainer height={200}>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src={this.state.bestReaderInfo.image}
+                    style={{ width: 100, height: 100 }}
+                  />
+                </ResponsiveContainer>
               </Paper>
             </Grid>
             <Grid item xs={12} md={3}>
@@ -231,7 +273,7 @@ class AuthorDashboard extends React.Component {
                       fill="#8884d8"
                       label={"asd"}
                     >
-                      {data.map((entry, index) => (
+                      {this.state.howManyReadersChart.map((entry, index) => (
                         <Cell fill={colors[index % colors.length]} />
                       ))}
                     </Pie>
@@ -241,49 +283,70 @@ class AuthorDashboard extends React.Component {
                 </ResponsiveContainer>
               </Paper>
             </Grid>
-            <Grid item xs={12} md={3}>
+            <Grid item xs={12} md={4}>
               <Paper className="paper">
                 <h1>
                   Ile przeczytano <br />
                   <small>Ile osób przeczytało ... książek</small>
                 </h1>
+                <ResponsiveContainer height={400}>
+                  <BarChart
+                    data={this.state.howManyRead.map(x => ({
+                      name: x.books,
+                      value: x.count
+                    }))}
+                    margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+                  >
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Bar
+                      dataKey="value"
+                      fill="#8884d8"
+                      shape={<TriangleBar />}
+                      label={{ position: "top" }}
+                    >
+                      {this.state.howManyRead.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                      ))}
+                    </Bar>
+                  </BarChart>
+                </ResponsiveContainer>
               </Paper>
             </Grid>
-            <Grid item xs={12} md={6}>
+            <Grid item xs={12} md={5}>
               <Paper className="paper">
                 <h1>
                   Popularność książek <br />
                   <small>Ile osób przeczytało książkę ...</small>
                 </h1>
-                <RadarChart
-                  cx={300}
-                  cy={250}
-                  outerRadius={150}
-                  width={600}
-                  height={500}
-                  data={this.state.booksPopularity}
-                >
-                  <PolarGrid />
-                  <PolarAngleAxis dataKey="title" />
-                  <PolarRadiusAxis
-                    angle={90 / this.state.booksPopularity.length}
-                  />
-                  <Radar
-                    name="Kobiety"
-                    dataKey="F"
-                    stroke="#F012BE"
-                    fill="#F012BE"
-                    fillOpacity={0.6}
-                  />
-                  <Radar
-                    name="Mężczyźni"
-                    dataKey="M"
-                    stroke="#FF851B"
-                    fill="#FF851B"
-                    fillOpacity={0.6}
-                  />
-                  <Legend />
-                </RadarChart>
+                <ResponsiveContainer height={400}>
+                  <RadarChart
+                    outerRadius={150}
+                    data={this.state.booksPopularity}
+                  >
+                    <PolarGrid />
+                    <PolarAngleAxis dataKey="title" />
+                    <PolarRadiusAxis
+                      angle={90 / this.state.booksPopularity.length}
+                    />
+                    <Radar
+                      name="Kobiety"
+                      dataKey="F"
+                      stroke="#F012BE"
+                      fill="#F012BE"
+                      fillOpacity={0.6}
+                    />
+                    <Radar
+                      name="Mężczyźni"
+                      dataKey="M"
+                      stroke="#FF851B"
+                      fill="#FF851B"
+                      fillOpacity={0.6}
+                    />
+                    <Legend />
+                  </RadarChart>
+                </ResponsiveContainer>
               </Paper>
             </Grid>
           </Grid>
